374b6a6ad72426461a5a65e9efa22f586804e823
pwebb2002 <pwebb4@huskers.unl.edu>
2020-08-23 13:39:49 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Payton Webb
+ * Date : 2020/08/18
+ *
+ * Hello world program in c
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello World!\n");
+
+    return 0;
+}
